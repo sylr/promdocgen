bef5d2562a33fa27cf4247a5570e8b0e24c86bdb
Sylvain Rabot <sylvain@abstraction.fr>
2020-12-24 17:17:11 +0100

    Add more types
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 )
 
 func main() {
+	go func() {
+
+	}()
+
 	fset := token.NewFileSet()
 
 	src, err := os.Open(os.Args[1])
@@ -37,117 +41,161 @@ func fprint(composite bool, level int, format string, args ...interface{}) {
 }
 
 func printDeclRecursive(decl interface{}, level int, composite bool) {
-	switch decl.(type) {
+	switch typeddecl := decl.(type) {
 	case []ast.Decl:
-		decls := decl.([]ast.Decl)
-		for _, d := range decls {
+		for _, d := range typeddecl {
 			fmt.Println("--------")
 			printDeclRecursive(d, level, true && level > 0)
 		}
 	case []ast.Expr:
-		exprs := decl.([]ast.Expr)
-		for _, e := range exprs {
+		for _, e := range typeddecl {
+			printDeclRecursive(e, level, true && level > 0)
+		}
+	case []ast.Stmt:
+		for _, e := range typeddecl {
 			printDeclRecursive(e, level, true && level > 0)
 		}
 	case []interface{}:
-		exprs := decl.([]interface{})
-		for _, e := range exprs {
+		for _, e := range typeddecl {
 			printDeclRecursive(e, level, true && level > 0)
 		}
+	case *ast.GoStmt:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Call:\n", typeddecl)
+		printDeclRecursive(typeddecl.Call, level+2, false)
 	case *ast.ArrayType:
-		arr := decl.(*ast.ArrayType)
-		fprint(composite, level, "%T\n", arr)
-		fprint(composite, level+1, "%T Elt\n", arr)
-		printDeclRecursive(arr.Elt, level+2, composite)
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(composite, level+1, "%T Elt:\n", typeddecl)
+		printDeclRecursive(typeddecl.Elt, level+2, composite)
 	case *ast.FuncDecl:
-		fun := decl.(*ast.FuncDecl)
-		fprint(composite, level, "%T name=%s\n", fun, fun.Name)
-		fprint(composite, level+1, "%T Body:\n", fun)
-		printDeclRecursive(fun.Body, level+2, false)
+		fprint(composite, level, "%T name=%s\n", typeddecl, typeddecl.Name)
+		fprint(composite, level+1, "%T Body:\n", typeddecl)
+		printDeclRecursive(typeddecl.Body, level+2, false)
 	case *ast.BlockStmt:
-		block := decl.(*ast.BlockStmt)
-		fmt.Printf("%*s%T[]:\n", level*2, "", block)
-		for _, l := range block.List {
-			printDeclRecursive(l, level+1, true)
+		fprint(composite, level, "%T:\n", typeddecl)
+		fprint(composite, level+1, "%T List[]:\n", typeddecl)
+		for _, l := range typeddecl.List {
+			printDeclRecursive(l, level+2, true)
 		}
 	case *ast.ReturnStmt:
-		ret := decl.(*ast.ReturnStmt)
-		fprint(composite, level, "%T\n", ret)
-		fprint(false, level+1, "%T Results[]:\n", ret)
-		for _, r := range ret.Results {
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Results[]:\n", typeddecl)
+		for _, r := range typeddecl.Results {
 			printDeclRecursive(r, level+2, true)
 		}
 	case *ast.ExprStmt:
-		expr := decl.(*ast.ExprStmt)
-		fprint(composite, level, "%+#v\n", expr)
-		fprint(false, level+1, "ast.ExprStmt X\n")
-		printDeclRecursive(expr.X, level+2, false)
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T X\n", typeddecl)
+		printDeclRecursive(typeddecl.X, level+2, false)
+	case *ast.AssignStmt:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Lhs\n", typeddecl)
+		printDeclRecursive(typeddecl.Lhs, level+2, false)
+		fprint(false, level+1, "%T Rhs\n", typeddecl)
+		printDeclRecursive(typeddecl.Rhs, level+2, false)
 	case *ast.CallExpr:
-		cexpr := decl.(*ast.CallExpr)
-		fprint(composite, level, "%T\n", cexpr)
-		fprint(false, level+1, "%T Fun:\n", cexpr)
-		printDeclRecursive(cexpr.Fun, level+2, false)
-		fprint(false, level+1, "%T Args[]:\n", cexpr)
-		printDeclRecursive(cexpr.Args, level+2, true)
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Fun:\n", typeddecl)
+		printDeclRecursive(typeddecl.Fun, level+2, false)
+		fprint(false, level+1, "%T Args[]:\n", typeddecl)
+		printDeclRecursive(typeddecl.Args, level+2, true)
 	case *ast.SelectorExpr:
-		sexpr := decl.(*ast.SelectorExpr)
-
-		x, okx := sexpr.X.(*ast.Ident)
-		if okx && x.Obj == nil && sexpr.Sel.Obj == nil {
-			//fmt.Printf("%*sast.SelectorExpr %s.%s\n", level*2, "", x.Name, sexpr.Sel.Name)
-			fprint(composite, level, "%T %s.%s\n", sexpr, x.Name, sexpr.Sel.Name)
+		x, okx := typeddecl.X.(*ast.Ident)
+		if okx && x.Obj == nil && typeddecl.Sel.Obj == nil {
+			fprint(composite, level, "%T %s.%s\n", typeddecl, x.Name, typeddecl.Sel.Name)
 		} else {
-			fprint(composite, level, "%T\n", sexpr)
-			fprint(composite, level+1, "%T X:\n", sexpr)
-			printDeclRecursive(sexpr.X, level+2, false)
-			fprint(composite, level+1, "%T Sel:\n", sexpr)
-			printDeclRecursive(sexpr.Sel, level+2, false)
+			fprint(composite, level, "%T\n", typeddecl)
+			fprint(false, level+1, "%T X:\n", typeddecl)
+			printDeclRecursive(typeddecl.X, level+2, false)
+			fprint(false, level+1, "%T Sel:\n", typeddecl)
+			printDeclRecursive(typeddecl.Sel, level+2, false)
 		}
 	case *ast.KeyValueExpr:
-		kvexpr := decl.(*ast.KeyValueExpr)
-
-		key, ok := kvexpr.Key.(*ast.Ident)
-		value, ok2 := kvexpr.Value.(*ast.BasicLit)
+		key, ok := typeddecl.Key.(*ast.Ident)
+		value, ok2 := typeddecl.Value.(*ast.BasicLit)
 		if ok && ok2 {
 			fprint(composite, level, "ast.KeyValueExpr key=%+#v value=%v\n", key.Name, value.Value)
 		} else {
-			fprint(composite, level, "%T\n", kvexpr)
-			fprint(false, level+1, "%T Key:\n", kvexpr)
-			printDeclRecursive(kvexpr.Key, level+2, false)
-			fprint(false, level+1, "%T Value:\n", kvexpr)
-			printDeclRecursive(kvexpr.Value, level+2, false)
+			fprint(composite, level, "%T\n", typeddecl)
+			fprint(false, level+1, "%T Key:\n", typeddecl)
+			printDeclRecursive(typeddecl.Key, level+2, false)
+			fprint(false, level+1, "%T Value:\n", typeddecl)
+			printDeclRecursive(typeddecl.Value, level+2, false)
 		}
 	case *ast.CompositeLit:
-		compol := decl.(*ast.CompositeLit)
-		fprint(composite, level, "ast.CompositeLit\n")
-		fprint(false, level+1, "ast.CompositeLit Type:\n")
-		printDeclRecursive(compol.Type, level+2, false)
-		fprint(false, level+1, "ast.CompositeLit Elts[]:\n")
-		printDeclRecursive(compol.Elts, level+2, true)
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Type:\n", typeddecl)
+		printDeclRecursive(typeddecl.Type, level+2, false)
+		fprint(false, level+1, "%T Elts[]:\n", typeddecl)
+		printDeclRecursive(typeddecl.Elts, level+2, true)
 	case *ast.BasicLit:
-		basicl := decl.(*ast.BasicLit)
-		fprint(composite, level, "ast.BasicLit kind=%+#v value=%v\n", basicl.Kind.String(), basicl.Value)
+		fprint(composite, level, "ast.BasicLit kind=%+#v value=%v\n", typeddecl.Kind.String(), typeddecl.Value)
+	case *ast.TypeSwitchStmt:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Init:\n", typeddecl)
+		printDeclRecursive(typeddecl.Init, level+2, false)
+		fprint(false, level+1, "%T Body:\n", typeddecl)
+		printDeclRecursive(typeddecl.Body, level+2, false)
+	case *ast.CaseClause:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Body:\n", typeddecl)
+		printDeclRecursive(typeddecl.Body, level+2, false)
+		fprint(false, level+1, "%T List[]:\n", typeddecl)
+		printDeclRecursive(typeddecl.List, level+2, true)
+	case *ast.StarExpr:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T X:\n", typeddecl)
+		printDeclRecursive(typeddecl.X, level+2, false)
+	case *ast.ParenExpr:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T X:\n", typeddecl)
+		printDeclRecursive(typeddecl.X, level+2, false)
+	case *ast.IndexExpr:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T X:\n", typeddecl)
+		printDeclRecursive(typeddecl.X, level+2, false)
+	case *ast.RangeStmt:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Value:\n", typeddecl)
+		printDeclRecursive(typeddecl.Value, level+2, false)
+		fprint(false, level+1, "%T Body:\n", typeddecl)
+		printDeclRecursive(typeddecl.Body, level+2, false)
+	case *ast.IfStmt:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Cond:\n", typeddecl)
+		printDeclRecursive(typeddecl.Cond, level+2, false)
+		fprint(false, level+1, "%T Body:\n", typeddecl)
+		printDeclRecursive(typeddecl.Body, level+2, false)
+		fprint(false, level+1, "%T Else:\n", typeddecl)
+		printDeclRecursive(typeddecl.Else, level+2, false)
+	case *ast.BinaryExpr:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T X:\n", typeddecl)
+		printDeclRecursive(typeddecl.X, level+2, false)
+		fprint(false, level+1, "%T Op: %s\n", typeddecl, typeddecl.Op.String())
+		fprint(false, level+1, "%T Y:\n", typeddecl)
+		printDeclRecursive(typeddecl.Y, level+2, false)
 	case *ast.Ident:
-		ident := decl.(*ast.Ident)
-		if ident.Obj != nil {
-			fprint(composite, level, "%T name=%s\n", ident, ident.Name)
-			printDeclRecursive(ident.Obj, level+1, false)
-		} else {
-			fprint(composite, level, "%+#v\n", ident)
+		fprint(composite, level, "%T name=%s\n", typeddecl, typeddecl.Name)
+		if typeddecl.Obj != nil {
+			fprint(false, level+1, "%T Object: %#+v\n", typeddecl, typeddecl.Obj)
 		}
 	case *ast.Object:
-		object := decl.(*ast.Object)
-		fprint(composite, level, "%+#v\n", object)
+		fprint(composite, level, "%T name=%s type=%v\n", typeddecl, typeddecl.Name, typeddecl.Type)
+		if typeddecl.Decl != nil {
+			fprint(false, level+1, "%T Decl: %+#v\n", typeddecl, typeddecl.Decl)
+			printDeclRecursive(typeddecl.Decl, level+2, false)
+		}
+		if typeddecl.Data != nil {
+			fprint(false, level+1, "%T Data: %+#v\n", typeddecl, typeddecl.Data)
+		}
 	case ast.Stmt:
-		stat := decl.(ast.Stmt)
-		fprint(composite, level, "%+#v\n", stat)
+		fprint(composite, level, "%+#v\n", typeddecl)
 	case ast.Expr:
-		expr := decl.(ast.Expr)
-		fprint(composite, level, "%T %+#v\n", expr, expr)
+		fprint(composite, level, "%T %+#v\n", typeddecl, typeddecl)
 	case ast.Decl:
-		decl := decl.(ast.Decl)
-		fprint(composite, level, "%T %+#v\n", decl, decl)
+		fprint(composite, level, "%T %+#v\n", typeddecl, typeddecl)
 	default:
-		fprint(composite, level, "%T %+#v\n", decl, decl)
+		fprint(composite, level, "%+#v\n", typeddecl)
 	}
 }
