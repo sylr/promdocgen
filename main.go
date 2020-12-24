5166407807ba48ac1854d8dc9ea85210268d06b4
Sylvain Rabot <sylvain@abstraction.fr>
2020-12-24 17:54:20 +0100

    Add more types
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- main.go
+++ main.go
@@ -55,10 +55,27 @@ func printDeclRecursive(decl interface{}, level int, composite bool) {
 		for _, e := range typeddecl {
 			printDeclRecursive(e, level, true && level > 0)
 		}
+	case []ast.Spec:
+		for _, e := range typeddecl {
+			printDeclRecursive(e, level, true && level > 0)
+		}
+	case []*ast.Ident:
+		for _, e := range typeddecl {
+			printDeclRecursive(e, level, true && level > 0)
+		}
 	case []interface{}:
 		for _, e := range typeddecl {
 			printDeclRecursive(e, level, true && level > 0)
 		}
+
+	case *ast.DeclStmt:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Decl:\n", typeddecl)
+		printDeclRecursive(typeddecl.Decl, level+2, false)
+	case *ast.GenDecl:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T Specs:\n", typeddecl)
+		printDeclRecursive(typeddecl.Specs, level+2, false)
 	case *ast.GoStmt:
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T Call:\n", typeddecl)
@@ -89,16 +106,18 @@ func printDeclRecursive(decl interface{}, level int, composite bool) {
 		printDeclRecursive(typeddecl.X, level+2, false)
 	case *ast.AssignStmt:
 		fprint(composite, level, "%T\n", typeddecl)
-		fprint(false, level+1, "%T Lhs\n", typeddecl)
-		printDeclRecursive(typeddecl.Lhs, level+2, false)
-		fprint(false, level+1, "%T Rhs\n", typeddecl)
-		printDeclRecursive(typeddecl.Rhs, level+2, false)
+		fprint(false, level+1, "%T Lhs[]:\n", typeddecl)
+		printDeclRecursive(typeddecl.Lhs, level+2, true)
+		fprint(false, level+1, "%T Rhs[]:\n", typeddecl)
+		printDeclRecursive(typeddecl.Rhs, level+2, true)
 	case *ast.CallExpr:
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T Fun:\n", typeddecl)
 		printDeclRecursive(typeddecl.Fun, level+2, false)
-		fprint(false, level+1, "%T Args[]:\n", typeddecl)
-		printDeclRecursive(typeddecl.Args, level+2, true)
+		if len(typeddecl.Args) > 0 {
+			fprint(false, level+1, "%T Args[]:\n", typeddecl)
+			printDeclRecursive(typeddecl.Args, level+2, true)
+		}
 	case *ast.SelectorExpr:
 		x, okx := typeddecl.X.(*ast.Ident)
 		if okx && x.Obj == nil && typeddecl.Sel.Obj == nil {
@@ -126,8 +145,10 @@ func printDeclRecursive(decl interface{}, level int, composite bool) {
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T Type:\n", typeddecl)
 		printDeclRecursive(typeddecl.Type, level+2, false)
-		fprint(false, level+1, "%T Elts[]:\n", typeddecl)
-		printDeclRecursive(typeddecl.Elts, level+2, true)
+		if len(typeddecl.Elts) > 0 {
+			fprint(false, level+1, "%T Elts[]:\n", typeddecl)
+			printDeclRecursive(typeddecl.Elts, level+2, true)
+		}
 	case *ast.BasicLit:
 		fprint(composite, level, "ast.BasicLit kind=%+#v value=%v\n", typeddecl.Kind.String(), typeddecl.Value)
 	case *ast.TypeSwitchStmt:
@@ -140,8 +161,12 @@ func printDeclRecursive(decl interface{}, level int, composite bool) {
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T Body:\n", typeddecl)
 		printDeclRecursive(typeddecl.Body, level+2, false)
-		fprint(false, level+1, "%T List[]:\n", typeddecl)
-		printDeclRecursive(typeddecl.List, level+2, true)
+		if len(typeddecl.List) > 0 {
+			fprint(false, level+1, "%T List[]:\n", typeddecl)
+			printDeclRecursive(typeddecl.List, level+2, true)
+		}
+
+	// X
 	case *ast.StarExpr:
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T X:\n", typeddecl)
@@ -154,6 +179,11 @@ func printDeclRecursive(decl interface{}, level int, composite bool) {
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T X:\n", typeddecl)
 		printDeclRecursive(typeddecl.X, level+2, false)
+	case *ast.TypeAssertExpr:
+		fprint(composite, level, "%T\n", typeddecl)
+		fprint(false, level+1, "%T X:\n", typeddecl)
+		printDeclRecursive(typeddecl.X, level+2, false)
+
 	case *ast.RangeStmt:
 		fprint(composite, level, "%T\n", typeddecl)
 		fprint(false, level+1, "%T Value:\n", typeddecl)
@@ -178,7 +208,23 @@ func printDeclRecursive(decl interface{}, level int, composite bool) {
 	case *ast.Ident:
 		fprint(composite, level, "%T name=%s\n", typeddecl, typeddecl.Name)
 		if typeddecl.Obj != nil {
-			fprint(false, level+1, "%T Object: %#+v\n", typeddecl, typeddecl.Obj)
+			//fprint(false, level+1, "%T Object: %#+v\n", typeddecl, typeddecl.Obj)
+		}
+	case *ast.ImportSpec:
+		fprint(composite, level, "%T name=%s value=%v\n", typeddecl, typeddecl.Name.String(), typeddecl.Path.Value)
+		if typeddecl.Comment != nil {
+			fprint(false, level+1, "%T Comment:\n", typeddecl)
+			printDeclRecursive(typeddecl.Comment, level+2, false)
+		}
+	case *ast.ValueSpec:
+		fprint(composite, level, "%T type=%s\n", typeddecl, typeddecl.Type)
+		if len(typeddecl.Names) > 0 {
+			fprint(false, level+1, "%T Names:\n", typeddecl)
+			printDeclRecursive(typeddecl.Names, level+2, false)
+		}
+		if len(typeddecl.Values) > 0 {
+			fprint(false, level+1, "%T Values:\n", typeddecl)
+			printDeclRecursive(typeddecl.Values, level+2, false)
 		}
 	case *ast.Object:
 		fprint(composite, level, "%T name=%s type=%v\n", typeddecl, typeddecl.Name, typeddecl.Type)
